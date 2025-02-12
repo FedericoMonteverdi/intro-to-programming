591d54df35bd0f34a5eb681ae579c672990865d1
Federico Monteverdi <federico_monteverdi@progressive.com>
2025-02-12 15:53:36 -0600

    progress

---
+++ src/week1/string-calculator-weds/StringCalculator/Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        if (numbers == "") {
+            return 0;
+        }
+        
+        if (numbers.Length > 0 && numbers.Length < 10 && !numbers.Contains(",") && !numbers.Contains("\n"))
+        {
+            return int.Parse(numbers);
+        }
+
+        char[] delimeters = new char[] { ',', '\n' };
+
+        if (numbers.Contains(","))
+        {
+            string[] parts = numbers.Split(',');
+            int sum = 0;
+            foreach (string part in parts)
+            {
+                sum += int.Parse(part);
+            }
+            return sum;
+        }
+
+
+        return 0;
+    }
+}
