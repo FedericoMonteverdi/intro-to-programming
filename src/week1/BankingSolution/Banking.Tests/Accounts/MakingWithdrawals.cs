7bbe968a2965ecfc65d2f3bffe636fc51d0a12f9
Federico Monteverdi <federico_monteverdi@progressive.com>
2025-02-12 14:25:49 -0600

    test cases

---
+++ src/week1/BankingSolution/Banking.Tests/Accounts/MakingWithdrawals.cs
@@ -0,0 +1,19 @@
+﻿using Banking.Domain;
+
+namespace Banking.Tests.Accounts;
+public class MakingWithdrawals
+{
+
+    [Fact]
+    public void MakingWithdrawalsDecreasesTheBalance()
+    {
+        var account = new Account();
+        var openingBalance = account.GetBalance();
+        var amountToWithdraw = 42.23M;
+
+        account.Withdraw(amountToWithdraw);
+
+        Assert.Equal(openingBalance - amountToWithdraw,
+            account.GetBalance());
+    }
+}
