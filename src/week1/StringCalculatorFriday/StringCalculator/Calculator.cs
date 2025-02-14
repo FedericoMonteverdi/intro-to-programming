c21398077b1d895ad019dd3876ef1722c0e10512
Federico Monteverdi <federico_monteverdi@progressive.com>
2025-02-14 16:13:05 -0600

    mixed delimeter test case passes

---
+++ src/week1/StringCalculatorFriday/StringCalculator/Calculator.cs
@@ -0,0 +1,28 @@
+﻿
+public class Calculator
+{
+    public int Add(string numbers)
+    {
+        if (numbers == "")
+        {
+            return 0;
+        }
+
+        if (numbers.Length > 0 && numbers.Length < 10 && !numbers.Contains(",") && !numbers.Contains("\n"))
+        {
+            return int.Parse(numbers);
+        }
+
+        char[] delimeters = new char[] { ',', '\n' };
+
+        string[] parts = numbers.Split(delimeters);
+        int sum = 0;
+        foreach (string part in parts)
+        {
+            sum += int.Parse(part);
+        }
+        return sum;
+        
+        //return 0;
+    }
+}
---
+++ src/week1/StringCalculatorFriday/StringCalculator/CalculatorTests.cs
@@ -0,0 +1,46 @@
+﻿
+namespace StringCalculator;
+public class CalculatorTests
+{
+    [Fact]
+    public void EmptyStringReturnsZero()
+    {
+        var calculator = new Calculator();
+
+        var result = calculator.Add("");
+
+        Assert.Equal(0, result);
+    }
+
+    [Theory]
+    [InlineData("1", 1)]
+    [InlineData("10", 10)]
+    public void SingleDigit(string numbers, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(numbers);
+        Assert.Equal(expected, result);
+    }
+
+    [Theory]
+    [InlineData("1,1", 2)]
+    [InlineData("1,2,3,4,5,6,7,8,9", 45)]
+    public void AddsIntsSeperatedByCommas(string numbers, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(numbers);
+        Assert.Equal(expected, result);
+
+    }
+
+    [Theory]
+    [InlineData("1\n2", 3)]
+    [InlineData("1\n2,3", 6)]
+    public void AddsIntsSeperatedByMixedDelimeter(string numbers, int expected)
+    {
+        var calculator = new Calculator();
+        var result = calculator.Add(numbers);
+        Assert.Equal(expected, result);
+
+    }
+}
